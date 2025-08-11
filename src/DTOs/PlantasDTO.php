3f1d8bb8f39b8802293388db2ccf92284747e9cd
Danil0-1 <danilomuskus1@gmail.com>
2025-08-11 14:07:07 +0000

    feat: :sparkles: DTO

--- src/DTOs/PlantasDTO.php
+++ src/DTOs/PlantasDTO.php
@@ -12,9 +12,15 @@ class PlantasDTO
     public static function fromArray(array $data): array
     {
         try {
+            v::key('tipo', v::in(['CLIENTE', 'PROVEEDOR', 'SOCIO']))
+	        ->key('fecha_registro', v::date('Y-m-d'))
+            ->assert($data);
         } catch (ValidationException $e) {
-            throw new InvalidArgumentException('Campos invalidos');
+            throw new InvalidArgumentException('Campos invalidos | tipo = CLIENTE o PROVEEDOR o SOCIO ');
         }
+
+	    $data['fecha_registro'] = (new DateTime($data['fecha_registro']))->format('Y-m-d');
+
 
         return $data; // datos ya validados
     }
